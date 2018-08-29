e01f055d087bbaa5cbd543fe9b7a2e6869f83a5f
kys <1409915687@qq.com>
2018-08-29 20:54:46 +0800

    update

--- myabtis_base/src/main/java/com/kyssion/SpringMybatisMain.java
+++ myabtis_base/src/main/java/com/kyssion/SpringMybatisMain.java
@@ -3,15 +3,66 @@ package com.kyssion;
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.apache.ibatis.type.BaseTypeHandler;
+import org.apache.ibatis.type.JdbcType;
+import org.apache.ibatis.type.MappedJdbcTypes;
+import org.apache.ibatis.type.TypeHandler;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import java.io.InputStream;
+import java.sql.CallableStatement;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class SpringMybatisMain {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext("classpath:spring.xml");
-        InputStream inputStream = Resources.getResourceAsStream("");
-        SqlSessionFactory sqlSessionFactory =
-                new SqlSessionFactoryBuilder().build(inputStream);
+    }
+}
+@MappedJdbcTypes()
+class Handle extends BaseTypeHandler<String>{
+
+    @Override
+    public void setNonNullParameter(PreparedStatement preparedStatement, int i, String s, JdbcType jdbcType) throws SQLException {
+
+    }
+
+    @Override
+    public String getNullableResult(ResultSet resultSet, String s) throws SQLException {
+        return null;
+    }
+
+    @Override
+    public String getNullableResult(ResultSet resultSet, int i) throws SQLException {
+        return null;
+    }
+
+    @Override
+    public String getNullableResult(CallableStatement callableStatement, int i) throws SQLException {
+        return null;
+    }
+}
+
+class Handle2 implements TypeHandler<String>{
+
+    @Override
+    public void setParameter(PreparedStatement preparedStatement, int i, String s, JdbcType jdbcType) throws SQLException {
+
+    }
+
+    @Override
+    public String getResult(ResultSet resultSet, String s) throws SQLException {
+        return null;
+    }
+
+    @Override
+    public String getResult(ResultSet resultSet, int i) throws SQLException {
+        return null;
+    }
+
+    @Override
+    public String getResult(CallableStatement callableStatement, int i) throws SQLException {
+        return null;
     }
 }
