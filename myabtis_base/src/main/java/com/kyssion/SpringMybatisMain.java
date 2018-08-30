448e0f995f6a2ebac4c3f47ded11f29bbbd2727a
kys <1409915687@qq.com>
2018-08-30 19:41:07 +0800

    update

--- myabtis_base/src/main/java/com/kyssion/SpringMybatisMain.java
+++ myabtis_base/src/main/java/com/kyssion/SpringMybatisMain.java
@@ -1,14 +1,17 @@
 package com.kyssion;
 
 import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.Configuration;
+import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-
 import java.io.InputStream;
 
 public class SpringMybatisMain {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext("classpath:spring.xml");
+        SqlSessionFactory factory = new SqlSessionFactoryBuilder().build(new Configuration());
+        SqlSession sqlSession = factory.openSession();
     }
 }
--- mybatis_test/src/main/java/com/kyssion/mapper/TestMapper.java
+++ mybatis_test/src/main/java/com/kyssion/mapper/TestMapper.java
@@ -1,6 +1,6 @@
-package kyssion.mapper;
+package com.kyssion.mapper;
 
-import kyssion.bean.TestBean;
+import com.kyssion.bean.TestBean;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 import org.springframework.stereotype.Repository;
@@ -10,4 +10,7 @@ import org.springframework.stereotype.Repository;
 public interface TestMapper {
     @Select("select * from mybatis_test where id = #{id}")
     TestBean getMapperById(TestBean bean);
+
+    @Select("select * from xm_test_item where id = #{id}")
+    TestBean getMapperById2(TestBean bean);
 }
