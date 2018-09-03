20b391fb23b7ee0e639acdb57021c23e9c6eaad3
kys <1409915687@qq.com>
2018-09-03 21:50:08 +0800

    upte

--- mybatis_test/src/main/java/com/kyssion/MybatisTestApplication.java
+++ mybatis_test/src/main/java/com/kyssion/MybatisTestApplication.java
@@ -2,6 +2,8 @@ package com.kyssion;
 
 import com.kyssion.service.TestService;
 import org.apache.ibatis.reflection.wrapper.ObjectWrapperFactory;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -19,7 +21,8 @@ public class MybatisTestApplication implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-//        System.out.println(testService.test().getName());
-        System.out.println(testService.test2().getName());
+        System.out.println(testService.test().getName());
+        SqlSession
+//        System.out.println(testService.test2().getName());
     }
 }
