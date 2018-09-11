c716ca25086e61b1140a72b45ce0a054a30a58c7
kys <1409915687@qq.com>
2018-09-11 20:59:14 +0800

    update

--- myabtis_base/src/main/java/com/kyssion/SpringMybatisMain.java
+++ myabtis_base/src/main/java/com/kyssion/SpringMybatisMain.java
@@ -10,13 +10,18 @@ import org.apache.ibatis.type.BaseTypeHandler;
 import org.apache.ibatis.type.JdbcType;
 import org.apache.ibatis.type.MappedJdbcTypes;
 import org.apache.ibatis.type.TypeHandler;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.SqlSessionTemplate;
 import org.mybatis.spring.annotation.MapperScan;
+import org.mybatis.spring.mapper.MapperFactoryBean;
+import org.mybatis.spring.mapper.MapperScannerConfigurer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import java.io.IOException;
 import java.io.InputStream;
 import java.sql.CallableStatement;
 import java.sql.PreparedStatement;
@@ -35,5 +40,19 @@ public class SpringMybatisMain implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         mapper.selectOne("sdfsdf");
+    }
+
+    public void test() throws IOException {
+        InputStream inputStream = Resources.getResourceAsStream("map/TestMapper.xml");
+        SqlSessionFactory factory = new SqlSessionFactoryBuilder().build(inputStream);
+        SqlSession sqlsession = factory.openSession();
+        TestMapper mapper = sqlsession.getMapper(TestMapper.class);
+    }
+
+    public void test2() throws Exception {
+        SqlSessionFactory factoryBean = new SqlSessionFactoryBean().getObject();
+        MapperFactoryBean mapperFactoryBean = new MapperFactoryBean();
+        SqlSessionTemplate sqlSessionTemplate = new SqlSessionTemplate(factoryBean);
+        MapperScannerConfigurer mapperScaner = new MapperScannerConfigurer();
     }
 }
